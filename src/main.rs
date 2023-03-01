35aafe9e3179e4b6c55582cbc9e96519209d7884
Robin Lu <wwng@2333.me>
2023-03-01 20:42:18 +0800

    add nonblocking socket

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use std::time;
+use std::io;
 use std::thread;
+use std::io::ErrorKind;
 use std::net::UdpSocket;
+use std::time::Duration;
 
-fn main() -> std::io::Result<()> {
-    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind to address");
+fn main() -> io::Result<()> {
+    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:0")?;
     socket.set_broadcast(true)?;
+    socket.set_read_timeout(Some(Duration::from_millis(100)))?;
+    socket.set_nonblocking(true).unwrap();
     loop {
-        socket
-            .send_to("BREED:ABORT".as_bytes(), "255.255.255.255:37540")
-            .expect("Couldn't send data");
-        println!("Send abort per 500ms.");
-        let mut buf = [0; 32];
-        let (number_of_bytes, _) = socket.recv_from(&mut buf).expect("Didn't receive data");
-        if number_of_bytes > 0 {
-            return Ok(());
-        } else {
-            thread::sleep(time::Duration::from_micros(500));
+        socket.send_to("BREED:ABORT".as_bytes(), "255.255.255.255:37540")?;
+        println!("Send abort command to breed per 500ms.");
+        let mut buf = [0; 14];
+        match socket.recv_from(&mut buf) {
+            Ok(_) => {
+                println!("Recv pong from breed.");  
+                return Ok(());
+            },
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                thread::sleep(Duration::from_secs_f32(0.5));
+                continue;
+            },
+            Err(_) => {},
         }
     }
 }
