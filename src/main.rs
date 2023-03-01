6a563c39d161b1b840e61a1b6c4f3460d4df7a08
Robin Lu <wwng@2333.me>
2023-03-01 20:49:27 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::io;
-use std::thread;
-use std::io::ErrorKind;
-use std::net::UdpSocket;
-use std::time::Duration;
 
-fn main() -> io::Result<()> {
-    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:0")?;
+fn main() -> std::io::Result<()> {
+    let socket = std::net::UdpSocket::bind("0.0.0.0:0")?;
     socket.set_broadcast(true)?;
-    socket.set_read_timeout(Some(Duration::from_millis(100)))?;
     socket.set_nonblocking(true).unwrap();
     loop {
         socket.send_to("BREED:ABORT".as_bytes(), "255.255.255.255:37540")?;
@@ -18,8 +12,8 @@ fn main() -> io::Result<()> {
                 println!("Recv pong from breed.");  
                 return Ok(());
             },
-            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                thread::sleep(Duration::from_secs_f32(0.5));
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                std::thread::sleep(std::time::Duration::from_secs_f32(0.5));
                 continue;
             },
             Err(_) => {},
