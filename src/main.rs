da57f4c3e570d1db8dea39a6a1e6d45c08017ef6
Robin Lu <wwng@2333.me>
2023-03-02 14:31:54 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ fn main() -> std::io::Result<()> {
     let socket = std::net::UdpSocket::bind("0.0.0.0:37540")?;
     socket.set_broadcast(true)?;
     socket.set_nonblocking(true).unwrap();
+    println!("Send abort command to breed per 500ms.");
     loop {
         socket.send_to("BREED:ABORT".as_bytes(), "255.255.255.255:37541")?;
-        println!("Send abort command to breed per 500ms.");
         let mut buf = [0; 14];
         match socket.recv_from(&mut buf) {
             Ok(_) => {
