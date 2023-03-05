39c9a056a56d737e9ea44dd79d80e98b55b89a8c
Robin Lu <wwng@2333.me>
2023-03-05 15:35:58 +0800

    ci

--- ci/build_and_test.bat
+++ ci/build_and_test.bat
@@ -5,8 +5,8 @@ set "RUSTFMT_CI=1"
 rustc -Vv || exit /b 1
 cargo -V || exit /b 1
 
-:: Build and test main crate
+:: Build
 cargo build --release || exit /b 1
 dir || exit /b 1
 tree || exit /b 1
-ls /d/a/breed-enter-rust/breed-enter-rust || exit /b 1
+ls D:\a\breed-enter-rust\breed-enter-rust\target\x86_64-pc-windows-msvc\release || exit /b 1
