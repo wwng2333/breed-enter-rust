59eec6133079ab7d92c95685e14ddf8052737ac0
Robin Lu <wwng@2333.me>
2023-10-11 12:43:04 +0800

    main.rs: add show local ip

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,26 @@ use std::io::Read;
 use std::net::UdpSocket;
 use std::thread;
 use std::time::Duration;
+
+fn localip_get() -> Option<String> {
+    let socket = match UdpSocket::bind("0.0.0.0:0") {
+        Ok(s) => s,
+        Err(_) => return None,
+    };
+
+    match socket.connect("192.168.1.1:80") {
+        Ok(()) => (),
+        Err(_) => return None,
+    };
+
+    match socket.local_addr() {
+        Ok(addr) => return Some(addr.ip().to_string()),
+        Err(_) => return None,
+    };
+}
+
 fn main() -> std::io::Result<()> {
+    println!("Local IP: {}", localip_get().unwrap());
     let socket = UdpSocket::bind("0.0.0.0:37540")?;
     socket.set_broadcast(true)?;
     socket.set_nonblocking(true).unwrap();
